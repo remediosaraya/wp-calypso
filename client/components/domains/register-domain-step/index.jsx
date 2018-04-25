d5ff1fe15cea2636916f5a761620d5119999245a
Jason Moon <jason.moon@automattic.com>
2018-04-25 16:56:26 -0600

    Domains: Create a feature flag for the TLD filter

--- client/components/domains/register-domain-step/index.jsx
+++ client/components/domains/register-domain-step/index.jsx
@@ -957,6 +957,8 @@ class RegisterDomainStep extends React.Component {
 			suggestions = this.props.defaultSuggestions || [];
 		}
 
+		const showTldFilterBar = config.isEnabled( 'domains/kracken-ui/tld-filter' );
+
 		return (
 			<DomainSearchResults
 				key="domain-search-results" // key is required for CSS transition of content/
@@ -982,7 +984,7 @@ class RegisterDomainStep extends React.Component {
 				fetchAlgo={ fetchAlgo }
 				cart={ this.props.cart }
 			>
-				{ config.isEnabled( 'domains/kracken-ui/filters' ) && (
+				{ showTldFilterBar && (
 					<TldFilterBar
 						availableTlds={ this.state.availableTlds }
 						filters={ this.state.filters }
